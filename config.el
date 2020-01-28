d7f80bf800c2365ef7c889427098766cec1094cd
HaiwenZhang <haiwensummer@gmail.com>
2020-01-28 15:48:41 +0800

    Haiwen branch init

--- config.el
+++ config.el
@@ -1,320 +1,54 @@
-;;; private/rschmukler/config.el -*- lexical-binding: t; -*-
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-(def-package! org
-  :config
-  (setq org-agenda-files (file-expand-wildcards "~/data/org/*.org"))
-  (setq org-directory (expand-file-name "~/data/org"))
-  (setq org-cycle-separator-lines 1)
-  (defvar +org-dir (expand-file-name "~/data/org"))
-  (setq org-capture-templates
-        '(("c" "Code Task" entry (file+headline "~/data/org/main.org" "Coding Tasks")
-           "* TODO %?\n  Entered on: %U - %a\n")
-          ("t" "Task" entry (file+headline "~/data/org/main.org" "Tasks")
-           "* TODO %?\n  Entered on: %U")
-          ("n" "Note" entry (file+datetree "~/data/org/main.org")
-           "* %?\n\n"))))
-
-(after! neotree
-  (setq doom-themes-neotree-file-icons 'icons)
-  (setq doom-themes-neotree-enable-file-icons 'icons)
-  (setq neo-theme 'icons))
-
-(after! company
-  (setq company-idle-delay 0))
-
-(after! doom-themes
-  (setq doom-neotree-file-icons t))
-
-(after! ivy
-  (setq ivy-re-builders-alist
-        '((t . ivy--regex-ignore-order)))
-
-  (setq ivy-initial-inputs-alist
-        '((counsel-minor . "")
-          (counsel-package . "")
-          (counsel-org-capture . "")
-          (counsel-M-x . "")
-          (counsel-describe-function . "")
-          (counsel-describe-variable . ""))))
-
-(after! projectile
-  (setq projectile-create-missing-test-files t)
-  (projectile-register-project-type 'haskell-stack '("stack.yaml")
-                                    :compile "stack build"
-                                    :test "stack build --test"
-                                    :test-suffix "Test")
-  (projectile-mode)
-  (projectile-load-known-projects))
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; refresh' after modifying this file!
 
 
-(add-hook! elixir-mode
-  (flycheck-mode)
-  (rainbow-delimiters-mode))
+;; These are used for a number of things, particularly for GPG configuration,
+;; some email clients, file templates and snippets.
+(setq user-full-name "Haiwen Zhang"
+      user-mail-address "haiwensummer@gmail.com")
 
-(add-hook! elm-mode
-  (flycheck-mode))
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+(setq doom-font (font-spec :family "monospace" :size 15))
 
-(def-package! rust-mode
-  :mode "\\.rs$"
-  :config
-  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
-  (setq rust-format-on-save t)
-  (flycheck-mode))
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. These are the defaults.
+(setq doom-theme 'doom-one)
 
-(after! rustic
-  (setq rustic-format-on-save t))
+;; If you intend to use org, it is recommended you change this!
+(setq org-directory "~/org/")
 
-(def-package! dockerfile-mode
-  :mode "Dockerfile$")
-
-
-(after! ranger
-  :config
-  (setq ranger-show-literal nil))
-
-
-(def-package! reason-mode
-  :mode "\\.re$"
-  :hook
-  (before-save . refmt))
-
-;; (add-hook! flycheck-rust
-;;   :after rust-mode
-;;   :config
-;;   (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
-
-;; (def-package! racer
-;;   :after rust-mode
-;;   :config
-;;   (setq racer-rust-src-path
-;;         (concat
-;;          (replace-regexp-in-string "\n$" "" (shell-command-to-string "rustc --print sysroot"))
-;;          "/lib/rustlib/src/rust/src"))
-;;   (company-mode)
-;;   (eldoc-mode))
-
-(def-package! flycheck-mix
-  :after elixir-mode
-  :config
-  (add-hook 'flycheck-mode-hook #'flycheck-mix-setup))
-
-(def-package! flycheck-credo
-  :after elixir-mode
-  :config
-  (setq flycheck-elixir-credo-strict t)
-  (add-hook 'flycheck-mode-hook #'flycheck-credo-setup))
-
-(def-package! erlang
-  :mode "\\.erl$"
-  :config
-  (erlang-mode))
-
-(def-package! racket-mode
-  :mode "\\.rkt$"
-  :config
-  (company-mode)
-  (flycheck-mode)
-  (rainbow-delimiters-mode))
-
-(def-package! aggressive-indent
-  :hook
-  (clojure-mode . aggressive-indent-mode)
-  (hy-mode . aggressive-indent-mode)
-  (lisp-mode . aggressive-indent-mode))
-
-(def-package! helm-cider
-  :hook (cider-mode . helm-cider-mode))
-
-(def-package! flycheck-clj-kondo
-  :after clojure-mode
-  :config
-  ;; (dolist (checkers '((clj-kondo-clj . clojure-joker)
-  ;;                   (clj-kondo-cljs . clojurescript-joker)
-  ;;                   (clj-kondo-cljc . clojure-joker)
-  ;;                   (clj-kondo-edn . edn-joker)))
-  ;; (flycheck-add-next-checker (car checkers) (cons 'error (cdr checkers)))))
-  )
-
-(after! clojure-mode
-  (define-clojure-indent
-    (PUT 2)
-    (POST 2)
-    (GET 2)
-    (PATCH 2)
-    (DELETE 2)
-    (context 2)
-    (for-all 2)
-    (checking 3)
-    (>defn :defn)
-    (>defn- :defn)
-    (match 1)
-    (cond 0)
-    (case 1)
-    (describe 1)
-    (it 2)
-    (fn-traced :defn)
-    (defn-traced :defn)
-    (assert-match 1))
-  (put '>defn 'clojure-doc-string-elt 2)
-  (put '>defn- 'clojure-doc-string-elt 2)
-  (put 'defn-traced 'clojure-doc-string-elt 2)
+;; If you want to change the style of line numbers, change this to `relative' or
+;; `nil' to disable it:
+(setq display-line-numbers-type t)
 
 
-  (defun rs/ig/restart ()
-    "Calls Integrant halt followed by integrant go"
-    (interactive)
-    (cider-interactive-eval "(do (integrant.repl/halt) (integrant.repl/go))"))
-
-  (defun rs/ig/reset ()
-    "Calls Integrant reset"
-    (interactive)
-    (cider-interactive-eval "(integrant.repl/reset)"))
-
-  (defun rs/user/sync-libs ()
-    "Calls Integrant reset"
-    (interactive)
-    (cider-interactive-eval "(user/sync-libs)"))
-
-
-  (setq clojure-align-forms-automatically t)
-  (setq cider-cljs-lein-repl
-        "(do (require 'figwheel-sidecar.repl-api)
-         (figwheel-sidecar.repl-api/start-figwheel!)
-         (figwheel-sidecar.repl-api/cljs-repl))")
-  (setq cljr-magic-require-namespaces
-        '(("io" . "clojure.java.io")
-          ("sh" . "clojure.java.shell")
-          ("jdbc" . "clojure.java.jdbc")
-          ("set" . "clojure.set")
-          ("time" . "java-time")
-          ("str" . "cuerdas.core")
-          ("path" . "pathetic.core")
-          ("walk" . "clojure.walk")
-          ("zip" . "clojure.zip")
-          ("async" . "clojure.core.async")
-          ("component" . "com.stuartsierra.component")
-          ("http" . "clj-http.client")
-          ("url" . "cemerick.url" )
-          ("sql" . "honeysql.core")
-          ("csv" . "clojure.data.csv")
-          ("json" . "cheshire.core")
-          ("s" . "clojure.spec.alpha")
-          ("fs" . "me.raynes.fs")
-          ("ig" . "integrant.core")
-          ("cp" . "com.climate.claypoole")
-          ("re-frame" . "re-frame.core")
-          ("rf"       . "re-frame.core")
-          ("re"       . "reagent.core")
-          ("reagent"  . "reagent.core")
-          ("u.core"   . "utopia.core")
-          ("gen" . "clojure.spec.gen.alpha"))))
-
-(def-package! graphql-mode
-  :mode "\\.gql$")
-
-(def-package! lsp-mode
-  :hook
-  (haskell-mode . lsp)
-  (python-mode . lsp)
-  (rustic-mode . lsp)
-  (reason-mode . lsp)
-  :config
-  (lsp-register-client
-   (make-lsp-client :new-connection (lsp-stdio-connection "reason-language-server")
-                    :major-modes '(reason-mode)
-                    :notification-handlers (ht ("client/registerCapability" 'ignore))
-                    :priority 1
-                    :server-id 'reason-ls))
-  :commands
-  lsp)
-
-(def-package! lsp-ui
-  :commands
-  lsp-ui-mode)
-
-(def-package! company-lsp
-  :commands company-lsp)
-
-(def-package! lsp-haskell
-  :after haskell-mode
-  :config
-  (setq lsp-haskell-process-path-hie "hie-wrapper"))
-
-(def-package! yapfify
-  :hook
-  (python-mode . yapf-mode)
-  (before-save . yapify-buffer))
-
-(after! org-babel
-  (add-hook 'org-babel-after-execute-hook 'org-display-inline-images 'append)
-  (org-babel-do-load-languages
-   'org-babel-load-languages
-   '((ipython . t))))
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', where Emacs
+;;   looks when you load packages with `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
+;; they are implemented.
 
 
-(def-package! haskell-mode
-  :mode "\\.hs$"
-  :config
-  (rainbow-delimiters-mode)
-  ;; (setq haskell-font-lock-symbols t)
-  (add-to-list ("<>" . "⊕"))
-  (setq haskell-font-lock-symbols-alist
-        (-reject
-         (lambda (elem)
-           (or))
-         ;; (string-equal "::" (car elem))))
-         haskell-font-lock-symbols-alist)))
-
-;; (def-package! flycheck-haskell
-;;   :hook (haskell-mode . flycheck-haskell-setup))
-
-(def-package! dhall-mode
-  :mode "\\.dhall$")
-
-(def-package! lispyville
-  :hook ((emacs-lisp-mode clojure-mode hy-mode json-mode) . lispyville-mode)
-  :config
-  (lispyville-set-key-theme
-   '(operators
-     c-w
-     prettify
-     text-objects
-     atom-movement
-     commentary
-     wrap
-     slurp/barf-lispy
-     additional
-     additional-movement
-     additional-insert
-     escape)))
-
-(def-package! alchemist
-  :after elixir-mode
-  :config
-  (defun rm/alchemist-project-toggle-file-and-tests ()
-    "Toggle between a file and its tests in the current window."
-    (interactive)
-    (if (alchemist-utils-test-file-p)
-        (alchemist-project-open-file-for-current-tests 'find-file)
-      (rm/alchemist-project-open-tests-for-current-file 'find-file)))
-
-  (defun rm/alchemist-project-open-tests-for-current-file (opener)
-    "Visit the test file for the current buffer with OPENER."
-    (let* ((filename (file-relative-name (buffer-file-name) (alchemist-project-root)))
-           (filename (replace-regexp-in-string "^lib/" "test/" filename))
-           (filename (replace-regexp-in-string "^web/" "test/" filename))
-           (filename (replace-regexp-in-string "^apps/\\(.*\\)/lib/" "apps/\\1/test/" filename))
-           (filename (replace-regexp-in-string "\.ex$" "_test\.exs" filename))
-           (filename (format "%s/%s" (alchemist-project-root) filename)))
-      (if (file-exists-p filename)
-          (funcall opener filename)
-        (if (y-or-n-p "No test file found; create one now?")
-            (alchemist-project--create-test-for-current-file
-             filename (current-buffer))
-          (message "No test file found."))))))
-
-(load! "+functions")
-(load! "+theming")
-(load! "+bindings")
-(load! "+commands")
-
+(setq evil-default-state 'emacs)
+(global-set-key "\C-s" 'swiper)
--- packages.el
+++ packages.el
@@ -1,32 +1,52 @@
 ;; -*- no-byte-compile: t; -*-
-;;; private/rschmukler/packages.el
+;;; $DOOMDIR/packages.el
 
-(package! flycheck)
-(package! flycheck-mix)
-(package! flycheck-credo)
-(package! flycheck-clojure)
-(package! erlang)
-(package! racket-mode)
-(package! geiser)
-(package! graphql-mode)
-(package! rust-mode)
-(package! racer)
-(package! flycheck-rust)
-(package! intero)
-(package! org)
-(package! lsp-mode)
-(package! lsp-ui)
-(package! lsp-haskell)
-(package! haskell-mode)
-(package! flycheck-haskell)
-(package! yapfify)
-(package! company-lsp)
-(package! dockerfile-mode)
-(package! aggressive-indent)
-(package! evil-matchit :recipe (:fetcher github :repo "redguardtoo/evil-matchit" :commit "7d65b4167b1f0086c2b42b3aec805e47a0d355c4"))
-(package! lispyville)
-(package! reason-mode)
-(package! ob-ipython)
-(package! flycheck-clj-kondo)
-(package! helm-cider)
-(package! dhall-mode)
+;; This is where you install packages, by declaring them with the `package!'
+;; macro, then running 'doom refresh' on the command line. You'll need to
+;; restart Emacs for your changes to take effect! Or at least, run M-x
+;; `doom/reload'.
+;;
+;; WARNING: Don't disable core packages listed in ~/.emacs.d/core/packages.el.
+;; Doom requires these, and disabling them may have terrible side effects.
+;;
+;; Here are a couple examples:
+
+
+;; All of Doom's packages are pinned to a specific commit, and updated from
+;; release to release. To un-pin all packages and live on the edge, do:
+;(setq doom-pinned-packages nil)
+
+;; ...but to unpin a single package:
+;(package! pinned-package :pin nil)
+
+
+;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
+;(package! some-package)
+
+;; To install a package directly from a particular repo, you'll need to specify
+;; a `:recipe'. You'll find documentation on what `:recipe' accepts here:
+;; https://github.com/raxod502/straight.el#the-recipe-format
+;(package! another-package
+;  :recipe (:host github :repo "username/repo"))
+
+;; If the package you are trying to install does not contain a PACKAGENAME.el
+;; file, or is located in a subdirectory of the repo, you'll need to specify
+;; `:files' in the `:recipe':
+;(package! this-package
+;  :recipe (:host github :repo "username/repo"
+;           :files ("some-file.el" "src/lisp/*.el")))
+
+;; If you'd like to disable a package included with Doom, for whatever reason,
+;; you can do so here with the `:disable' property:
+;(package! builtin-package :disable t)
+
+;; You can override the recipe of a built in package without having to specify
+;; all the properties for `:recipe'. These will inherit the rest of its recipe
+;; from Doom or MELPA/ELPA/Emacsmirror:
+;(package! builtin-package :recipe (:nonrecursive t))
+;(package! builtin-package-2 :recipe (:repo "myfork/package"))
+
+;; Specify a `:branch' to install a package from a particular branch or tag.
+;; This is required for some packages whose default branch isn't 'master' (which
+;; our package manager can't deal with; see raxod502/straight.el#279)
+;(package! builtin-package :recipe (:branch "develop"))
