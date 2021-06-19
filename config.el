89cada215c0213527da447854a1248016ba73735
HaiwenZhang <haiwensummer@gmail.com>
2021-06-19 20:29:02 +0800

    Add some config

--- config.el
+++ config.el
@@ -55,8 +55,9 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
-(load! "+keybinds")
-
-
-
 (add-hook 'window-setup-hook #'toggle-frame-maximized)
+
+
+(load! "+keybinds")
+(load! "+org")
+(load! "+langs")
